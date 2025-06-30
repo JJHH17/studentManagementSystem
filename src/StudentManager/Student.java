7d4133cd9c36065b9fa689ad1cdd48df56813553
James Hatfield <james.hatfield17@icloud.com>
2025-06-30 16:18:15 +0100

    Removed student ID variable and method from student class, managed through student manager class now

--- src/StudentManager/Student.java
+++ src/StudentManager/Student.java
@@ -6,19 +6,15 @@ public class Student {
     private int age;
     private int studentId = 0;
 
-    public Student(String firstName, String lastName, int age) {
+    public Student(String firstName, String lastName, int age, int studentID) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.studentId = (int) (Math.random() * 1_000);
+        this.studentId = studentID;
     }
 
     public String toString() {
         return "Name: " + this.firstName + " " + this.lastName + ", ID: " + this.studentId;
-    }
-
-    public int getID() {
-        return this.studentId;
     }
 
     public String getName() {
