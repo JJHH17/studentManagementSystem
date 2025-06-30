a749561099a73662a31579abf8c5be1bb9681f2f
James Hatfield <james.hatfield17@icloud.com>
2025-06-30 16:40:40 +0100

    Added method to remove student based on their given ID number

--- src/StudentManager/StudentManager.java
+++ src/StudentManager/StudentManager.java
@@ -3,18 +3,32 @@ package StudentManager;
 import java.util.ArrayList;
 
 public class StudentManager {
-    ArrayList<Student> studentList;
+    private ArrayList<Student> studentList;
+    private int studentID = 0;
 
     public StudentManager() {
         this.studentList = new ArrayList<>();
     }
 
     public void addStudent(String firstName, String lastName, int age) {
-        this.studentList.add(new Student(firstName, lastName, age));
+        this.studentList.add(new Student(firstName, lastName, age, ++this.studentID));
     }
 
     public void removeStudent(int id) {
-        // Add method to remove student based on ID
+        boolean found = false;
+
+        for (Student student : this.studentList) {
+            if (student.getID() == id) {
+                found = true;
+                this.studentList.remove(student);
+                System.out.println("Student ID: " + id + " was removed.");
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Student ID: " + id + " not found.");
+        }
     }
 
     public void removeStudent(String name) {
