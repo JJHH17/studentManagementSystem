d48e7df17465f9a3ff4c0b0cd2ecaeb407cce05b
James Hatfield <james.hatfield17@icloud.com>
2025-07-03 16:46:02 +0100

    Added scanner object, new interface object, entered program loop

--- src/StudentManager/Main.java
+++ src/StudentManager/Main.java
@@ -1,7 +1,12 @@
 package StudentManager;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserInterface newUser = new UserInterface(scanner);
 
+        newUser.start();
     }
 }
