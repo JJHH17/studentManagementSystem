24146bc0a638e3db4db719d5f29ddc6a6d812cae
James Hatfield <james.hatfield17@icloud.com>
2025-07-03 16:00:26 +0100

    Added loop section, allows user to add a student

--- src/StudentManager/UserInterface.java
+++ src/StudentManager/UserInterface.java
@@ -16,8 +16,22 @@ public class UserInterface {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("'Add' = Adds a new student | 'Remove Name' Removes student based on name | 'Remove ID' Removes student based on ID" +
-                    "");
+            System.out.println("'Add' = Adds a new student | 'Remove Name' Removes student based on name | 'Remove ID' Removes student based on ID");
+            String userInput = scanner.nextLine();
+
+            switch(userInput.toLowerCase()) {
+                case "add":
+                    System.out.println("Enter first name:");
+                    String firstName = scanner.nextLine();
+
+                    System.out.println("Enter last name:");
+                    String lastName = scanner.nextLine();
+
+                    System.out.println("Enter age:");
+                    int age = Integer.valueOf(scanner.nextLine());
+
+                    this.school.addStudent(firstName, lastName, age);
+            }
         }
     }
 }
