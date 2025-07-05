1c20ca43971c64c1001e6858f4ab9d3470d9779f
James Hatfield <james.hatfield17@icloud.com>
2025-07-05 16:13:32 +0100

    Refactored class name to prevent clash with imported module, added method to write to file

---
+++ src/StudentManager/FileWrite.java
@@ -0,0 +1,39 @@
+package StudentManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class FileWrite {
+    public FileWrite(String s) {
+
+    }
+
+    public void createFile() {
+        try {
+            File classroom = new File("../classroom.txt");
+            if (classroom.createNewFile()) {
+                System.out.println("File created: " + classroom.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void writeFile() {
+        try {
+            FileWriter fileWrite = new FileWriter("../classroom.txt");
+            fileWrite.write("This was successful");
+            fileWrite.close();
+            System.out.println("Wrote to file successfully.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred (File Write)");
+            e.printStackTrace();
+        }
+    }
+}
