8e024001880d25f84722d7fd34a394a7b1c8470c
James Hatfield <james.hatfield17@icloud.com>
2025-07-07 16:38:20 +0100

    Added method to read file

--- src/StudentManager/FileManager.java
+++ src/StudentManager/FileManager.java
@@ -1,9 +1,11 @@
 package StudentManager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileManager {
     private final String FILENAME = "classroom.txt"; // File constant for reading and writing
@@ -37,5 +39,26 @@ public class FileManager {
             System.out.println("An error occurred (File Write)");
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<Student> readFile() {
+        ArrayList<Student> students = new ArrayList<>();
+        try (Scanner fileScanner = new Scanner (new File(FILENAME))) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    students.add(new Student(
+                            parts[0], // Firstname
+                            parts[1], // Lastname
+                            Integer.parseInt(parts[2]), // Age
+                            Integer.parseInt(parts[3]) // id
+                    ));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No existing file found.");
+        }
+        return students;
     }
 }
