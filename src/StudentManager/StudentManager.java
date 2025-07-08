196b0cdc1c027ad27c1e79fed87b9db3d7ed7b95
James Hatfield <james.hatfield17@icloud.com>
2025-07-08 16:05:56 +0100

    Refactored method to remove students to and from file that we read

--- src/StudentManager/StudentManager.java
+++ src/StudentManager/StudentManager.java
@@ -4,28 +4,38 @@ import java.util.ArrayList;
 
 public class StudentManager {
     private ArrayList<Student> studentList;
-    private int studentID = 0;
+    private int highestID;
+    private FileManager fileManager;
 
-    public StudentManager() {
-        this.studentList = new ArrayList<>();
+    public StudentManager(FileManager fileManager) {
+        this.fileManager = fileManager;
+        this.studentList = fileManager.readFile();
+        // Finds the highest ID in the existing student list
+        this.highestID = 0;
+        for (Student student : this.studentList) {
+            if (student.getID() > this.highestID) {
+                this.highestID = student.getID();
+            }
+        }
     }
 
     public void addStudent(String firstName, String lastName, int age) {
-        this.studentList.add(new Student(firstName, lastName, age, ++this.studentID));
+        this.studentList.add(new Student(firstName, lastName, age, ++this.highestID));
+        fileManager.writeFile(this.studentList);
     }
 
     public void removeStudent(int id) {
         boolean found = false;
 
-        for (Student student : this.studentList) {
-            if (student.getID() == id) {
+        for (int i = 0; i < this.studentList.size(); i++) {
+            if (this.studentList.get(i).getID() == id) {
                 found = true;
-                this.studentList.remove(student);
-                System.out.println("Student ID: " + id + " was removed.");
+                this.studentList.remove(i);
+                System.out.println("Student ID: " + id + " removed.");
+                fileManager.writeFile(this.studentList);
                 break;
             }
         }
-
         if (!found) {
             System.out.println("Student ID: " + id + " not found.");
         }
@@ -34,16 +44,18 @@ public class StudentManager {
     public void removeStudent(String firstName, String lastName) {
         boolean found = false;
 
-        for (Student student : this.studentList) {
+        for (int i = 0; i < this.studentList.size(); i++) {
+            Student student = this.studentList.get(i);
             if (student.getName().equalsIgnoreCase(firstName + " " + lastName)) {
                 found = true;
-                this.studentList.remove(student);
+                studentList.remove(i);
+                System.out.println("Student: " + firstName + " " + lastName + " was removed.");
+                fileManager.writeFile(this.studentList);
                 break;
             }
         }
-
         if (!found) {
-            System.out.println("Student name: " + firstName + " " + lastName + " not found.");
+            System.out.println("Student: " + firstName + " " + lastName + " was not found.");
         }
     }
 
